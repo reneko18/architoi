e8efaa5bda4212f23177eed2b7b9f7d56aedc6d9
reneko18 <rpaniaguagarate@gmail.com>
2022-04-25 15:18:29 +0200

    Projet Architoi

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,47 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="index3.html" class="brand-link">
+    <img src="{{ url('adminlte/dist/img/favicon.png')}}" alt="Architoi Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">Architoi</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+
+  <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+        <li class="nav-item">
+          <a href="{{ route('users') }}" class="nav-link">
+            <i class="nav-icon fas fa-users"></i>
+            <p>
+              Utilisateurs
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{ route('projets') }}" class="nav-link">
+            <i class="nav-icon fas fa-building"></i>
+            <p>
+              Projets
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{ route('professions') }}" class="nav-link">
+            <i class="nav-icon fas fa-user-tie"></i>
+            <p>
+              Professions
+            </p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
